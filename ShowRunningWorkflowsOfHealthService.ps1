37f175f82043ae6c114f0d4cdef4ce64594c31e6
Udish Mudiar <34421872+Udish17@users.noreply.github.com>
2023-03-16 23:45:12 +0530

    Update ShowRunningWorkflowsOfHealthService.ps1

--- ShowRunningWorkflowsOfHealthService.ps1
+++ ShowRunningWorkflowsOfHealthService.ps1
@@ -1,3 +1,31 @@
+#=================================================================================
+# 
+# This is a workaround script which can be used to get the running workflows
+# in SCOM Health Service when the number of workflows are high and the default
+# task in SCOM times out aftr 30 minutes
+# 
+# The script has a pre-requisites to get the workflow from the edb database                   
+# using Stealth software.        
+# 
+# Below are the instructions:
+# 1. Stop the MMA on the server.
+# 2. Copy the HealthServiceStore.edb file from the installation directory. Like
+#    Management Server : C:\Program Files\Microsoft System Center\Operations Manager\Server\Health Service State\Health Service Store
+#    or Agent C:\Program Files\Microsoft Monitoring Agent\Agent\Health Service State\Health Service Store
+# 3. Open the edb file in Stealth.
+# 4. Navigate to the end on the left panel and expand WORKFLOW
+# 5. Click on Records. Note the number in parenthesis.
+# 6. Click on View -> Options -> Set the Mex Records to a number that you see under Records.
+# 7. Click on Edit -> Select All.
+# 8. Click on Edit -> Copy.
+# 9. Paste the clipboard in Excel Sheet.
+# 10. Delete all columns except D, L
+# 11. Inset a new row at the top. Add the name of the columns as InstanceID and WorkflowName.
+# 12. Save the file as .csv format.
+# 13. Copy the file full path and run the script with that as parameter.
+#
+#Author:  Udishman Mudiar
+# v 1.0
 #
 #=================================================================================
 
